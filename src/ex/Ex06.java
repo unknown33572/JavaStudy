4c9e5303a08312392e9a5b95bc2aa22b4392e319
S <s@MacBook-Pro-16.local>
2023-06-08 00:14:26 +0900

    array

---
+++ src/ex/Ex06.java
@@ -0,0 +1,17 @@
+package ex;
+
+public class Ex06 {
+  public static void main(String[] args) {
+    int[] score;
+    score = new int[5];
+    score[0] = 50;
+    score[1] = 60;
+    score[2] = 70;
+    score[3] = 80;
+    score[4] = 90;
+
+    for(int i = 0; i < score.length; i++) {
+      System.out.println("score[" + i + "] = " + score[i]);
+    }
+  }
+}
